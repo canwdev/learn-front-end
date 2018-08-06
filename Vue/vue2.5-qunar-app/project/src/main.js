73287f2f57b11fb5f9a51b6c48cd7c56ac9bee15
canwdev <c2097@live.com>
2018-08-06 21:24:31 +0800

    安装依赖

--- Vue/vue2.5-qunar-app/project/src/main.js
+++ Vue/vue2.5-qunar-app/project/src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/css/normalize.css'
+// 解决不同尺寸移动设备边框显示的问题
+import './assets/css/border.css'
+// 解决某些移动设备点击事件有300毫秒延迟的问题
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
