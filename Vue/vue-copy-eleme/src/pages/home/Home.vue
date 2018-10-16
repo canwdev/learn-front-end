1d1352f43cb965bdf64505d794ea19f9392f322c
canwdev <c2097@live.com>
2018-10-16 21:48:35 +0800

    home

---
+++ Vue/vue-copy-eleme/src/pages/home/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import vHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    vHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.app
+  .tab
+    display flex
+    justify-content space-around
+    background #ccc
+    .tab-item
+      line-height 45px
+      flex 1
+      text-align center
+</style>
---
+++ Vue/vue-copy-eleme/src/pages/home/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>This is header</div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
